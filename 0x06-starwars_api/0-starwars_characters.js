1d4445163962727119686ad263d511ec71758a5b
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-15 04:19:06 +0100

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,14 @@
 #!/usr/bin/node
-
-const request = require('request');
-
-const movieId = process.argv[2]; // Changed MovieId to movieId
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`; // Changed apiurl to apiUrl
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching movie:', error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-
-  characters.forEach(url => {
-    request(url, (error, response, body) => { // Changed response to request
-      if (error) {
-        console.error('Error fetching character:', error);
-        return;
+export default function getResponseFromAPI (success) {
+    return new Promise((resolve, reject) => {
+      if (success === true) {
+        const newObj = {
+          status: 200,
+          body: 'Success'
+        };
+        resolve(newObj);
+      } else {
+        reject(new Error('The fake API is not working currently'));
       }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
     });
-  });
-});
+  }
