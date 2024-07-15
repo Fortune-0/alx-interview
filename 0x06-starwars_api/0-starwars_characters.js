080fd240fba8d43765acf75a20af43ca9935ebe4
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-15 04:26:32 +0100

    update task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-export default function getResponseFromAPI (success) {
-    return new Promise((resolve, reject) => {
-      if (success === true) {
-        const newObj = {
-          status: 200,
-          body: 'Success'
-        };
-        resolve(newObj);
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
+export default function getFullResponseFromAPI (success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      const newObj = {
+        status: 200,
+        body: 'Success'
+      };
+      resolve(newObj);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
