ccaac5270bb587b91380299dd9d957e3ce4bfa8a
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-15 04:10:40 +0100

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-
-
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const movieId = process.argv[2]; // Changed MovieId to movieId
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`; // Changed apiurl to apiUrl
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error(error);
+  if (error) {
+    console.error('Error fetching movie:', error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+
+  characters.forEach(url => {
+    request(url, (error, response, body) => { // Changed response to request
+      if (error) {
+        console.error('Error fetching character:', error);
         return;
-    }
-
-    const data = JSON.parse(body);
-    const characters = data.characters;
-
-    characters.forEach((characterUrl) => {
-        request(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
-        });
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
+  });
 });
